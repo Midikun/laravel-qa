f5e55fbd761a2b89d2477d47952ece307e617bea
Sarmidi <sarmidi.204@knss.co.id>
2022-10-26 13:28:09 +0700

    Implement authorization using policy

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -10,6 +10,9 @@ use Illuminate\Support\Facades\Auth;
 
 class QuestionController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -69,9 +72,10 @@ class QuestionController extends Controller
      */
     public function edit(Question $question)
     {
-        if (\Gate::denies('update-question', $question)) {
+        $this->authorize("update", $question);
+       /* if (\Gate::denies('update-question', $question)) {
             abort(403, "Access denied");
-        }
+        }*/
         return view("questions.edit", compact('question'));
     }
 
@@ -84,9 +88,10 @@ class QuestionController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
-        if (\Gate::denies('update-question', $question)) {
+        $this->authorize("update", $question);
+       /* if (\Gate::denies('update-question', $question)) {
             abort(403, "Access denied");
-        }
+        }*/
         $question->update($request->only('title', 'body'));
 
         return redirect('/questions')->with('success', "Your question has been updated.");
@@ -100,9 +105,10 @@ class QuestionController extends Controller
     public function destroy(Question $question)
     {
         
-        if (\Gate::denies('delete-question', $question)) {
+        /*if (\Gate::denies('delete-question', $question)) {
             abort(403, "Access denied");
-        }
+        }*/
+        $this->authorize("delete", $question);
         $question->delete();
 
 
--- resources/views/questions/index2.blade.php
+++ resources/views/questions/index2.blade.php
@@ -35,16 +35,19 @@
                                 <div class="d-flex align-items-center">
                                     <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                     <div class="ml-auto">
-                                        @can ('update', $question)
-                                            <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                                        @endcan
-                                        @can ('delete', $question)
-                                            <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
-                                                @method('DELETE')
-                                                @csrf
-                                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                                            </form>
-                                        @endcan
+                                        @if (Auth::user()->can('update-question', $question))
+                                        @endif
+                                        <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                   
+
+                                    @if (Auth::user()->can('delete-question', $question))
+                                    @endif
+                                        <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
+                                            @method('DELETE')
+                                            @csrf
+                                            <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                        </form>
+                                   
                                     </div>
                                 </div>
                                 <p class="lead">
