e141bac16a3c2023bd2971380cb501f9b01681ef
Sarmidi <midikunudinus@gmail.com>
2022-11-01 21:03:08 +0700

    update best_answer_id when answer removed on table layer

--- app/Answer.php
+++ app/Answer.php
@@ -33,4 +33,13 @@ class Answer extends Model
             $answer->question->decrement('answers_count');            
         });       
     }
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
+    }
 }
